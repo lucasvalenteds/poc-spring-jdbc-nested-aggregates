278ae66e22a3514853963a80246a07de33423a03
Lucas <lucasvalenteds@gmail.com>
2022-11-04 22:03:08 -0300

    Provisining Postgres database for testing

--- src/test/java/com/example/ApplicationTest.java
+++ src/test/java/com/example/ApplicationTest.java
@@ -2,9 +2,27 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class ApplicationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> CONTAINER =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres"));
+
+    @DynamicPropertySource
+    private static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", CONTAINER::getUsername);
+        registry.add("spring.datasource.password", CONTAINER::getPassword);
+    }
 
     @Test
     void contextLoads() {
